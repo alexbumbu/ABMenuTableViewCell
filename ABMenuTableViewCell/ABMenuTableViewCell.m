839ff54edfb5289096af5205a1db76cfbf34370f
alexbumbu <alex.bumbu@gmail.com>
2015-02-18 13:44:38 +0200

    fix to avoid going in selected state when menu is shown

--- ABMenuTableViewCell/ABMenuTableViewCell.m
+++ ABMenuTableViewCell/ABMenuTableViewCell.m
@@ -8,22 +8,30 @@
 
 #import "ABMenuTableViewCell.h"
 
+typedef NS_ENUM(NSInteger, ABMenuUpdateAction) {
+    ABMenuUpdateShowAction = 1,
+    ABMenuUpdateHideAction = -1
+};
+
 @implementation ABMenuTableViewCell {
     CGRect _rightMenuViewInitialFrame;
+    UIPanGestureRecognizer *_swipeGesture;
 }
 
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
-        UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(swipeGesture:)];
-        panGesture.delegate = self;
-        [self addGestureRecognizer:panGesture];
+        _swipeGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(swipeGesture:)];
+        _swipeGesture.delegate = self;
+        [self addGestureRecognizer:_swipeGesture];
     }
     
     return self;
 }
 
 - (void)willMoveToSuperview:(UIView *)newSuperview {
+    [super willMoveToSuperview:newSuperview];
+    
     if (newSuperview) {
         // content view frame is different before moving to superview, so it's critical to get it right
         if (_rightMenuView) {
@@ -33,12 +41,21 @@
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
     // Configure the view for the selected state
-    if (selected) {
-        _rightMenuView.frame = CGRectMake(CGRectGetWidth(self.contentView.frame), .0, .0, CGRectGetHeight(self.contentView.frame));
+    if (CGRectGetWidth(_rightMenuView.frame) > 0) {
+        [self updateMenuView:ABMenuUpdateHideAction animated:YES];
+        return;
     }
+    
+    [super setSelected:selected animated:animated];
+}
+
+- (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated {
+    // check if the menu view is visible
+    if (CGRectGetWidth(_rightMenuView.frame) > 0)
+        return;
+    
+    [super setHighlighted:highlighted animated:animated];
 }
 
 - (void)layoutSubviews {
@@ -49,10 +66,14 @@
     return NO;
 }
 
-- (BOOL)gestureRecognizerShouldBegin:(UIPanGestureRecognizer *)panGestureRecognizer {
-    // enable only horizontal gesture
-    CGPoint velocity = [panGestureRecognizer velocityInView:self];
-    return fabs(velocity.x) > fabs(velocity.y);
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
+    if (gestureRecognizer == _swipeGesture) {
+        // enable only horizontal gesture
+        CGPoint velocity = [(UIPanGestureRecognizer*)gestureRecognizer velocityInView:self];
+        return fabs(velocity.x) > fabs(velocity.y);
+    }
+    
+    return YES;
 }
 
 - (void)setRightMenuView:(UIView *)rightMenuView {
@@ -73,46 +94,64 @@
 
 - (void)swipeGesture:(UIPanGestureRecognizer *)gesture {
     if (gesture.state == UIGestureRecognizerStateBegan) {
-        CGRect menuNewFrame;
-        CGFloat initialWidth = CGRectGetWidth(_rightMenuViewInitialFrame);
         NSInteger direction;
         
         // find swipe direction
         CGPoint velocity = [gesture velocityInView:self];
         if (velocity.x > 0) {
-            // towards right
-            menuNewFrame = CGRectMake(CGRectGetWidth(self.contentView.frame), .0, .0, CGRectGetHeight(self.contentView.frame));
-            direction = -1;
+            // towards right - hide menu view
+            direction = ABMenuUpdateHideAction;
         }
         else {
-            // towards left
-            menuNewFrame = CGRectMake(CGRectGetWidth(self.contentView.frame) - initialWidth, .0, initialWidth, CGRectGetHeight(self.contentView.frame));
-            direction = 1;
+            // towards left - show menu view
+            direction = ABMenuUpdateShowAction;
         }
         
-        if (CGRectEqualToRect(menuNewFrame, _rightMenuView.frame))
-            return;
+        [self updateMenuView:direction animated:YES];
+    }
+}
 
-        // animate showing menuView
-        [UIView animateWithDuration:.26
-                         animations:^{
-                             _rightMenuView.frame = menuNewFrame;
-                         }];
-        
-        for (UIView *subview in self.contentView.subviews) {
-            if (subview == _rightMenuView)
-                continue;
+
+#pragma mark Private Methods
+
+- (void)updateMenuView:(ABMenuUpdateAction)direction animated:(BOOL)animated {
+    CGRect menuNewFrame;
+    CGFloat initialWidth = CGRectGetWidth(_rightMenuViewInitialFrame);
+    
+    switch (direction) {
+        case ABMenuUpdateShowAction:
+            menuNewFrame = CGRectMake(CGRectGetWidth(self.contentView.frame) - initialWidth, .0, initialWidth, CGRectGetHeight(self.contentView.frame));
+            break;
+        case ABMenuUpdateHideAction:
+            menuNewFrame = CGRectMake(CGRectGetWidth(self.contentView.frame), .0, .0, CGRectGetHeight(self.contentView.frame));
+            break;
             
-            [UIView animateWithDuration:.6
-                                  delay:.0
-                 usingSpringWithDamping:(direction > 0) ? .6 : 1.0
-                  initialSpringVelocity:(direction > 0) ? 1.0 : .0
-                                options:0
-                             animations:^{
-                                 subview.frame = CGRectMake(CGRectGetMinX(subview.frame) - direction*initialWidth, CGRectGetMinY(subview.frame), CGRectGetWidth(subview.frame), CGRectGetHeight(subview.frame));
-                             }
-                             completion:nil];
-        }
+        default:
+            break;
+    }
+    
+    if (CGRectEqualToRect(menuNewFrame, _rightMenuView.frame))
+        return;
+    
+    // animate showing menuView
+    [UIView animateWithDuration:(animated? .26 : .0)
+                     animations:^{
+                         _rightMenuView.frame = menuNewFrame;
+                     }];
+    
+    for (UIView *subview in self.contentView.subviews) {
+        if (subview == _rightMenuView)
+            continue;
+        
+        [UIView animateWithDuration:(animated? .6 : .0)
+                              delay:.0
+             usingSpringWithDamping:(direction > 0) ? .6 : 1.0
+              initialSpringVelocity:(direction > 0) ? 1.0 : .0
+                            options:0
+                         animations:^{
+                             subview.frame = CGRectMake(CGRectGetMinX(subview.frame) - direction*initialWidth, CGRectGetMinY(subview.frame), CGRectGetWidth(subview.frame), CGRectGetHeight(subview.frame));
+                         }
+                         completion:nil];
     }
 }
 
