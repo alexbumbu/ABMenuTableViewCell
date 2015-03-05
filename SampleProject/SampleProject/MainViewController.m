0769e9d2fe9a71ad4d950ab24dbca62b482599da
Alex Bumbu <alex.bumbu@gmail.com>
2015-03-05 22:01:34 +0200

    load data source from plist & UI fixes;

--- SampleProject/SampleProject/MainViewController.m
+++ SampleProject/SampleProject/MainViewController.m
@@ -23,14 +23,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
     // Do any additional setup after loading the view from its nib.
     
-    // build data source
-    _dataSource = [[NSMutableArray alloc] initWithCapacity:20];
-    for (int i = 0; i < 20; i++) {
-        [_dataSource addObject:[NSString stringWithFormat:@"Lorem ipsum dolor sit amet %@", @(i + 1)]];
-    }
+    // load data source
+    [self loadDataSource];
     
     self.navigationItem.titleView = segmentedControl;
 }
@@ -53,25 +49,24 @@
 
 - (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *identifier = @"ABMenuTableViewCell";
-    ABMenuTableViewCell *cell = (ABMenuTableViewCell*)[tableView cellForRowAtIndexPath:indexPath];
     
+    ABMenuTableViewCell *cell = (ABMenuTableViewCell*)[tableView dequeueReusableCellWithIdentifier:identifier];
     if (!cell) {
-        cell = [[ABMenuTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
-        
-        // add menu view
-        ABCellMenuView *menuView = [ABCellMenuView initWithNib:(segmentedControl.selectedSegmentIndex == 0? @"ABCellMenuView" : @"ABCellMenuView2") bundle:nil];
-        menuView.delegate = self;
-        cell.rightMenuView = menuView;
+        cell = [[ABMenuTableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:identifier];
     }
     
     cell.textLabel.text = [_dataSource objectAtIndex:indexPath.row];
-    ((ABCellMenuView*)cell.rightMenuView).indexPath = indexPath;
+    cell.detailTextLabel.textColor = [UIColor darkGrayColor];
+    cell.detailTextLabel.text = @"swipe to show custom menu";
+    
+    // custom menu view
+    NSString *nibName = (segmentedControl.selectedSegmentIndex == 0)? @"ABCellMailStyleMenuView" : @"ABCellCustomStyleMenuView";
+    ABCellMenuView *menuView = [ABCellMenuView initWithNib:nibName bundle:nil];
+    menuView.delegate = self;
+    menuView.indexPath = indexPath;
+    cell.rightMenuView = menuView;
     
     return cell;
-}
-
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    return YES;
 }
 
 
@@ -79,14 +74,25 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+//    NSLog(@"1 %@", 12);
 }
 
 
 #pragma mark ABCellMenuViewDelegate Methods
 
-- (void)cellMenuViewEditBtnTapped:(ABCellMenuView *)menuView {
+- (void)cellMenuViewFlagBtnTapped:(ABCellMenuView *)menuView {
     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@""
-                                                    message:[NSString stringWithFormat:@"Edit button pressed at position %@", @(menuView.indexPath.row)]
+                                                    message:[NSString stringWithFormat:@"Flag button pressed at position %@", @(menuView.indexPath.row)]
+                                                   delegate:nil
+                                          cancelButtonTitle:@"Ok"
+                                          otherButtonTitles:nil];
+    [alert show];
+}
+
+- (void)cellMenuViewMoreBtnTapped:(ABCellMenuView *)menuView {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@""
+                                                    message:[NSString stringWithFormat:@"More button pressed at position %@", @(menuView.indexPath.row)]
                                                    delegate:nil
                                           cancelButtonTitle:@"Ok"
                                           otherButtonTitles:nil];
@@ -96,13 +102,17 @@
 - (void)cellMenuViewDeleteBtnTapped:(ABCellMenuView *)menuView {
     // update data source
     [_dataSource removeObjectAtIndex:menuView.indexPath.row];
-
-    // update UI
-    ABMenuTableViewCell *cell = (ABMenuTableViewCell*)[listTableView cellForRowAtIndexPath:menuView.indexPath];
-    cell.rightMenuView = nil;
     
-    [listTableView deleteRowsAtIndexPaths: @[menuView.indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
-    [listTableView reloadData];
+    // update UI
+    [listTableView deleteRowsAtIndexPaths: @[menuView.indexPath] withRowAnimation:UITableViewRowAnimationLeft];
+
+    // make sure to reload in order to update the custom menu index path for each row
+    NSMutableArray *rowsToReload = [NSMutableArray array];
+    for (int i = 0; i < _dataSource.count - menuView.indexPath.row; i++) {
+        [rowsToReload addObject:[NSIndexPath indexPathForRow:menuView.indexPath.row + i inSection:0]];
+    }
+    
+    [listTableView reloadRowsAtIndexPaths:rowsToReload withRowAnimation:UITableViewRowAnimationAutomatic];
 }
 
 
@@ -110,6 +120,14 @@
 
 - (IBAction)segmentedControlValueChanged:(UISegmentedControl *)sender {
     [listTableView reloadData];
+}
+
+
+#pragma mark Private Methods
+
+- (void)loadDataSource {
+    NSString *dataSourcePath = [[NSBundle mainBundle] pathForResource:@"DataSource" ofType:@"plist"];
+    _dataSource = [[NSMutableArray alloc] initWithContentsOfFile:dataSourcePath];
 }
 
 @end
